f199b83641b6b78326be98d3b7d2a455e5005933
Pariya Jankhani <pariya.jan2021@gmail.com>
2025-11-06 17:24:09 +0330

    Day2
    Create a Student Management System for learning OOP, Class, Capsulation ,Lists, Array ...

---
+++ Day2/StudentManagementSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentManagementSystem
+{
+
+     class Student
+    {
+
+        //Property
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+
+        //Constractor
+       public Student(string name, int id)
+        {
+
+        }
+    }
+
+     class SchoolManager
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
