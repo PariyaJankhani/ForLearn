f81c403009c16454fe88a5ad8e45277428562c41
Pariya Jankhani <pariya.jan2021@gmail.com>
2025-11-09 16:19:16 +0330

    Complete Student/Grade Models and Add Test UI

--- Day2/StudentManagementSystem/Program.cs
+++ Day2/StudentManagementSystem/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace StudentManagementSystem
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+using StudentManagementSystem;
+
+namespace StudentManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("please enter id");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("please enter age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter course name");
+            string course = Console.ReadLine();
+
+            Console.WriteLine($"please enter grade of {course} :");
+           
+            int score = int.Parse(Console.ReadLine());
+
+           Student student = new Student(id, name, age);
+
+            student.AddGrade(course, score);
+
+
+            Console.WriteLine($"Student ID: {student.Id}");
+            Console.WriteLine($"Student Name: {student.Name}");
+            Console.WriteLine($"Student Age: {student.Age}");
+            foreach (var grade in student.Grades)
+            {
+                Console.WriteLine($"Course: {grade.CourseName}, Score: {grade.Score}");
+            }
+
         }
     }
 }
--- Day2/StudentManagementSystem/Student.cs
+++ Day2/StudentManagementSystem/Student.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace StudentManagementSystem
 {
-    internal class Student
+    public class Student
     {
+        public int Id { get;  set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public List<Grade> Grades { get; set; }
+
+        public Student(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            PhoneNumber = "";
+            Grades = new List<Grade>();
+
+        }
+
+
+        public void AddGrade(string courseName, int score)
+        {
+            Grades.Add(new Grade(courseName, score));
+        }
     }
+
+    public class Grade
+    {
+        public string CourseName { get; set; }
+        public int Score { get; set; }
+
+        public Grade(string courseName, int score)
+        {
+            CourseName = courseName;
+            Score = score;
+        }
+
+
+
+    }
+
+   
+
+
 }
